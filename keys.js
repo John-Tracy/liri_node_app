f7887e5e4202971e22d644150b001f0480f60c09
John Tracy <johnmtracy133@hotmail.com>
2016-06-05 23:24:14 -0400

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
+  consumer_key: '',
+  consumer_secret: '',
   access_token_key: '<input here>',
   access_token_secret: '<input here>',
 }
