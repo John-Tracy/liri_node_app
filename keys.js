23e84b9510bbaff327a7988e7f120d52bc800d23
John Tracy <johnmtracy133@hotmail.com>
2016-06-05 23:25:59 -0400

    delete key file

--- keys.js
+++
@@ -1,8 +0,0 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
-}
